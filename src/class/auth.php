11ac892d2ca05c81a356c610f6e631a1738f29d3
ely <elychirivella10@gmail.com>
2022-05-09 16:20:50 -0400

    s

--- src/class/auth.php
+++ src/class/auth.php
@@ -21,7 +21,7 @@ class Auth {
                 break;
             
             default:
-                $roles = ["leer", "eliminar", "actualizar", "agregar"];
+                $roles = ["leer", "actualizar", "agregar"];
                 break;
         }
 
